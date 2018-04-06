8199fe938e7ea8b6f160e9a5495d7c8793afaba5
Gene <Gjansuy@gmail.com>
2018-04-06 02:58:30 -0700

    CHECKPOINT, BIG BEAUTIFUL RED SCREEN
    Vertical Sync needed to be matched to different timings.
    
    Currently Using these timings: https://learn.digilentinc.com/Documents/269

--- clock50HzTo25MHz.v
+++ clock50HzTo25MHz.v
@@ -2,16 +2,16 @@ module downclocker50to25(clk_50mhz, clk_25hz);
 input clk_50mhz;
 output clk_25hz;
 reg clk_25hz;
-reg [24:0] count;
+reg [1:0] count;
 always @ (posedge clk_50mhz)
 begin
-if(count == 1) begin
-count <= 0;
-$dumpfile("f.vcd");
-clk_25hz <= ~clk_25hz;
-end
-else begin
-count <= count + 1;
-end
+	if(count == 1) begin
+		count <= 0;
+		$dumpfile("f.vcd");
+		clk_25hz <= ~clk_25hz;
+	end
+	else begin
+	count <= 1;
+	end
 end
 endmodule
--- testPLL_sim/synopsys/vcsmx/vcsmx_setup.sh
+++ testPLL_sim/synopsys/vcsmx/vcsmx_setup.sh
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 17.1 593 win32 2018.04.06.01:33:36
+# ACDS 17.1 593 win32 2018.04.06.02:25:08
 
 # ----------------------------------------
 # vcsmx - auto-generated simulation script
@@ -107,7 +107,7 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 17.1 593 win32 2018.04.06.01:33:36
+# ACDS 17.1 593 win32 2018.04.06.02:25:08
 # ----------------------------------------
 # initialize variables
 TOP_LEVEL_NAME="testPLL"
